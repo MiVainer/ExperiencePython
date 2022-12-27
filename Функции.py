f64f3cc572bd3bebacebed77d3ab7742d5ddb0aa
Mihail Vainer <mh93@yandex.ru>
2022-12-27 22:48:24 +0700

    Функции ver.1.4

--- Функции.py
+++ Функции.py
@@ -1,10 +1,31 @@
-def function():
+def function(price):
     """Peres4itat ceny s y4etom nds"""
-    price = float(input("введи начальную цену - "))
     nds = 13/100
     new_price = price + nds*price
     print('новая цена -', new_price)
-print('------------------------------')
 
-function()
+def summa(x, y):
+    s = x + y
+    return s
 
+def factorial():
+    x = int(input('Введи значение факториала - !'))
+    fak = 1
+    for i in range(1, x+1):
+        fak = fak * i
+    return fak
+
+def stroka():
+    print('---------------')
+
+print('Функция № 1')
+function(110) # Вызвали функцию с величиной price = 110.
+stroka()
+
+print('Функция № 2')
+x = summa(10, 20)
+print(x)
+stroka()
+
+print('Функция № 3\nЗначение факториала -', factorial())
+stroka()
